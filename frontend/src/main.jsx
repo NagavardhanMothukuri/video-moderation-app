d38cd4da41eec4640220802840fb64857913e7ba
NagavardhanMothukuri <nagavardhanairforce@gmail.com>
2026-01-03 15:23:10 +0530

    Fix CSS import and dashboard UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css";
+import "./index.css";   // 🔴 THIS LINE IS REQUIRED
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
